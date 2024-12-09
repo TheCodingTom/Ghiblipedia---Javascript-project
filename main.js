92513a6f58ca202ae6a34ff7cf03c26b67a5c9b6
TheCodingTom <dastolto.t@gmail.com>
2024-12-09 10:56:50 +0100

    First cards created with bootstrap and JS!

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 console.log(movies);
 
-function displayTitles(titlesArray) {
-  const titlesContainer = document.querySelector(".movie-titles");
+function displayCards(filmsArray) {
+  const cardsContainer = document.querySelector(".cards-container");
 
-  for (let i = 0; i < titlesArray.length; i++) {
-    const movieTitle = document.createElement("h4");
-    movieTitle.innerText = titlesArray[i].title;
-    titlesContainer.appendChild(movieTitle);
+  for (let i = 0; i < filmsArray.length; i++) {
+    const cardContainer = document.createElement("div");
+    cardContainer.setAttribute("class", "card");
+    cardContainer.setAttribute("style", "width: 18rem;");
+
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("src", filmsArray[i].image); // using the index to select all elements of the array and remember the structure of the data I'm using
+    cardImage.setAttribute("alt", "image of a studio ghibli movie"); // double check if I find a better description
+
+    const cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.setAttribute("class", "card-title"); // alternative: cardTitle.classList.add("card-title")
+    cardTitle.innerText = filmsArray[i].title;
+
+    const cardText = document.createElement("p");
+    cardText.setAttribute("class", "card-text");
+    cardText.innerText = filmsArray[i].description;
+
+    const cardLink = document.createElement("a");
+    cardLink.setAttribute("href", "");
+    cardLink.setAttribute("class", "btn btn-primary");
+    cardLink.innerText = "Discover more";
+
+    cardsContainer.appendChild(cardContainer);
+    cardContainer.appendChild(cardImage);
+    cardContainer.appendChild(cardBody); // same level as the image - not a child of cardImage
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(cardLink);
   }
 }
 
-// displayTitles(movies);
+displayCards(movies);
