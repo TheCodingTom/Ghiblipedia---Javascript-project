76e9b3f5837d3ee429813293b7f8a9235fb1651f
TheCodingTom <dastolto.t@gmail.com>
2024-12-16 14:38:51 +0100

    Added a show more/show less button

--- index.js
+++ index.js
@@ -28,3 +28,26 @@ function changeColorEvent() {
 
 addButtonEvent();
 changeColorEvent();
+
+function showMoreText() {
+  const dots = document.getElementById("dots");
+  const moreText = document.getElementById("more");
+  const btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function showMoreEvent() {
+  const myButton = document.querySelector(".btn");
+  myButton.addEventListener("click", showMoreText);
+}
+
+showMoreEvent();
